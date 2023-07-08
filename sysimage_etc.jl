21f32a2032614d2e923be436ca4a4f0f07a8cdc5
qwjyh <urataw421@gmail.com>
2023-07-09 02:50:26 +0900

    remove Plots to fix compile failure

--- sysimage_etc.jl
+++ sysimage_etc.jl
@@ -1,7 +1,7 @@
 using PackageCompiler
 create_sysimage(
-	["OhMyREPL", "Plots", "CSV", "DataFrames", "GLMakie", "CairoMakie"],
+	[#="OhMyREPL", "Plots",=# "CSV", "DataFrames", "GLMakie", "CairoMakie"],
 	sysimage_path="sys-etc.so", 
 	precompile_execution_file=["precompile_plots.jl", "precompile_makie.jl"],
-	precompile_statements_file=["precompile_ohmyrepl.jl"] 
+	# precompile_statements_file=["precompile_ohmyrepl.jl"] 
 )
